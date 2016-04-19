240195fe17e5120a3946592ce80276242e12937e
Leonardo Hardtke <leohardtke@gmail.com.com>
2016-04-20 00:00:32 +1000

    Change setup reflecting the new program name

--- FourierExtraction/setup.py
+++ FourierExtraction/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='fourer_parameters_extract.py',
+    name='fourier.py',
     version='0.1',
-    py_modules=['fourer_parameters_extract.py'],
+    py_modules=['fourier'],
     install_requires=[
         'Click','gdal','numpy','scipy'
     ],
     entry_points='''
         [console_scripts]
-        fourer_parameters_extract.py=fourer_parameters_extract.py:cli
+        fourer.py=fourier.py:cli
     ''',
 )
