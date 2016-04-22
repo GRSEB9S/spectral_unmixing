a8b6beff2ce7139f5227da8d991efc75df427586
Leonardo Hardtke <leohardtke@gmail.com.com>
2016-04-22 16:59:41 +1000

    Working OK

--- clip_group_region.py
+++ clip_group_region.py
@@ -6,17 +6,20 @@ from grass.script.core import gisenv
 @click.argument('groupname',metavar='Imagery_group')
 @click.argument('subfix',metavar='Output_subfix')
 @click.argument('roi',metavar='Region_of interest')
-
+@click.argument('nullval',default=0,metavar='nullvalue')
+@click.option('--overwrite/--no-overwrite', default=False, help="Use --overwrite if the output file exists and you what to overwrite it.")
 
 def main(**kwargs):
     	""" This utility clips images of a gruop and create a gorup of the output files.
     	The output raster name will be like: originalname_subfix	  
     	Optionaly it set the null values of the output group
     
-	Example: clip_group_region LE72270902002150EDC00_toar cliped madryn 0"""
+	Example: clip_group_region LE72270902000209EDC00_toar madryn madryn --overwrite"""
     	roi = kwargs['roi']
 	groupname = kwargs['groupname']
 	subfix = kwargs['subfix']
+	nullval = kwargs['nullval']
+	overwrite = kwargs['overwrite']
     	gscript.run_command('g.region', region=roi)
     	grouplist=gscript.read_command("i.group", group=groupname, flags="g" ).split()
     	for raster in grouplist:
@@ -24,7 +27,10 @@ def main(**kwargs):
         	band= name.split(".")[1]
         	rout= name.split(".")[0]+"_"+subfix
         	expression="{rout}.{b} = {rin}".format(rin=name,rout=rout,b=band)
-        	gscript.mapcalc(expression)                                                          
+		if overwrite == True:
+				gscript.run_command('r.null', map="{rout}.{b}".format(rout=rout,b=band),setnull=nullval,overwrite=True)
+		if overwrite == False:		
+			gscript.mapcalc(expression)                                                          
         	gscript.run_command('r.null', map="{rout}.{b}".format(rout=rout,b=band),setnull=nullval)
     	mlist=gscript.list_grouped('raster', pattern=groupname+"_"+subfix+"*")[gisenv()['MAPSET']]
     	gscript.run_command("i.group", group=groupname+"_"+subfix, input=mlist)
